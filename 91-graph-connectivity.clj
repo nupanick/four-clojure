c751bdbe630a2c11cbe5dfff867217eed5c5e38a
Nicholas Lamicela <nupanick@gmail.com>
2017-11-07 00:20:46 +0000

    Added header and word wrap and stuff I guess

--- 91-graph-connectivity.clj
+++ 91-graph-connectivity.clj
@@ -1,16 +1,17 @@
 #!/usr/bin/clojure
 
-;; Given a graph, determine whether the graph is connected. A connected
-;; graph is such that a path exists between any two given nodes.
-;; - Your function must return true if the graph is connected and false
-;;   otherwise.
+;; 4clojure.com, problem 91
+;; Given a graph, determine whether the graph is connected. A
+;; connected graph is such that a path exists between any two given
+;; nodes.
+;; - Your function must return true if the graph is connected and
+;;   false otherwise.
 ;; - You will be given a set of tuples representing the edges of a
 ;;   graph. Each member of a tuple being a vertex/node in the graph.  
 ;; - Each edge is undirected (can be traversed either direction).
 
-(def f91
-  (fn [x]
-    x))
+(defn is-connected? [edges]
+  
 
 (defn run-tests [f]
   [(= true (f #{[:a :a]}))
